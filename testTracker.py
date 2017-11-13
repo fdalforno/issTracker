fa6c2739967b13a2ac8ede7bbff48e4c313e77f9
DalFornoF <fdalforno@gmail.com>
2017-11-13 20:39:11 +0100

    gestione passaggio nei punti di interesse

--- testTracker.py
+++ testTracker.py
@@ -1,8 +1,18 @@
 from Tracker import Tracker
-
+import ephem
+from datetime import datetime
 
 test = Tracker()
+test.retrieveFile()
 test.loadTle('stations.txt')
 test.loadSatellite('ISS (ZARYA)')
-point = test.getNetMaxLatitude()
-test.checkSunlight(point[0],point[1],point[2])
+prev = test.calcTable(10,False)
+
+print("Previsione passaggi alla ora UTC %s ora locale %s \n\n" % (ephem.date(datetime.utcnow()), ephem.date(datetime.now())))
+
+
+print("Orario,Longitudine,Latitudine,Illuminato,Interessante")
+
+for row in prev:
+    print(ephem.Date(row[0]),",",ephem.degrees(row[1]),",",ephem.degrees(row[2]),",",row[3],",",row[4])
+
