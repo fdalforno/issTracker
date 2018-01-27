e1020972350668466bc7344e2a1c27f5ab77c68e
Fabio Dal Forno <fdalforno@gmail.com>
2018-01-27 18:05:57 +0000

    aggiunti commenti

--- measure.py
+++ measure.py
@@ -2,6 +2,26 @@ from Tracker import Tracker
 from Data import Photo,Sensors
 import time
 
+'''
+Con questo progetto vogliamo fotografare le aurore boreali e misurare l'interazione del sole con
+il campo magnetico terrestre.
+
+Il progetto si compone di due parti la prima chiamata Tracker che serve per eseguire le previsioni
+di passaggio nella zona del Canada dove le aurore boreali si manifestano a latitudini piu' basse.
+La zona del Canada scelta e' compresa tra una longitudine di 100W e 80W come recuperato da https://en.wikipedia.org/wiki/Aurora.
+
+Per eseguire l'esperimento completamente dovremmo poter avere un'orbita che passa in quel quadrante di notte.
+(Possiamo provare a eseguire noi una previsione dei passaggi).
+
+La seconda parte del progetto si measure.py si occupa di eseguire le fotografie, misurare il campo magnetico e le accelerazioni
+dovute all'accensione dei motori.
+
+Ci aspettiamo essenzialmente un aumento dell'intensita' del campo magnetico all'avvicinarsi dei poli terrestri
+e una diminuzione significativa dello stesso nella parte non al sole della traiettoria della ISS.
+
+
+'''
+
 iss = Tracker()
 photo = Photo()
 sensor = Sensors()
@@ -10,38 +30,39 @@ iss.retrieveFile()
 iss.loadTle('stations.txt')
 iss.loadSatellite('ISS (ZARYA)')
 
-numeroMisura = 0
 
-while True:
-    max_diff = iss.optimizeDiff()
-    
-    
-    
-    if max_diff <= 0.05:
-        if numeroMisura > 2:
-            numeroMisura = 0
-            file = photo.captureImage()
-            sensor.write_sensors(iss,file)
-        else:
-            sensor.write_sensors(iss)
+contatoreMisura = 0
+
+try:
+    while True :
+        if iss.optimizeDiff() < 0.05:
             
-    else:
-        if numeroMisura > 60:
-            numeroMisura = 0
-            file = photo.captureImage()
-            sensor.write_sensors(iss,file)
+            #siamo nella parte alta/bassa dell'orbita facciamo le foto piu' spesso
+            if contatoreMisura > 10:
+                image = photo.captureImage()
+                contatoreMisura = 0
+                sensor.write_sensors(iss,image)
+            else:
+                sensor.write_sensors(iss)
+                
         else:
-            sensor.write_sensors(iss) 
+            
+            '''
+            in tutto il resto dell'orbita continuiamo lo stesso a fare foto per curiosita'
+            '''
+            if contatoreMisura > 120:
+                image = photo.captureImage()
+                contatoreMisura = 0
+                sensor.write_sensors(iss,image)
+            else:
+                sensor.write_sensors(iss)
+                
+        contatoreMisura = contatoreMisura +1
+        time.sleep(1)
+except (KeyboardInterrupt, SystemExit):
+    print('\n! Fine esperimento \n')
+    sensor.close()
+    photo.close()
+
     
     
-    numeroMisura += 1
-    time.sleep(1)
-    
-
-
-sensor.close()
-
-
-
-
-
