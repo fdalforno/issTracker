93470cbc04c376a6b4ad12e1e6be1449e5fce7c1
Fabio Dal Forno <fdalforno@gmail.com>
2018-01-27 10:19:41 +0000

    Gestione Previsione Passaggi

---
+++ createTable.py
@@ -0,0 +1,17 @@
+from Tracker import Tracker
+import ephem
+from datetime import datetime
+
+test = Tracker()
+test.retrieveFile()
+test.loadTle('stations.txt')
+test.loadSatellite('ISS (ZARYA)')
+prev = test.calcTable(10,False)
+
+print("Previsione passaggi alla ora UTC %s ora locale %s \n\n" % (ephem.date(datetime.utcnow()), ephem.date(datetime.now())))
+
+
+print("Orario,Longitudine,Latitudine,Illuminato,Interessante")
+
+for row in prev:
+    print(ephem.Date(row[0]),",",ephem.degrees(row[1]),",",ephem.degrees(row[2]),",",row[3],",",row[4])
